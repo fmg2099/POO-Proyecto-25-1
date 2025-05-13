4c9cbfc0a32d50ea63b4a7d5bf20369761c4528d
fmg2099 <fmg2099@lab502>
2025-05-13 16:35:43 -0600

    Ejemplo de Casts

--- include/EjemploConstCast.h
+++ include/EjemploConstCast.h
@@ -8,7 +8,7 @@ class EjemploConstCast
 	{
 		const int x = 10;
 		int* p = const_cast<int*>(&x); // Eliminar la constancia
-		x = 25; //no se puede porque lo marcamos como const
+		//dx = 25; //no se puede porque lo marcamos como const
 
 		*p = 20; // Modificar el valor apuntado
 		std::cout << "Valor de x: " << x << std::endl; // Imprime 20
